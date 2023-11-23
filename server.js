16ee2a9e166a439ae646eddfe66c23ce458e7244
Nugar <cladeadenugar@gmail.com>
2023-11-23 10:19:19 +0100

    Iniated file

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+//configurations
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+//File storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+//mongoDB connection
+const port = process.env.PORT || 5000;
+const mongoURL = process.env.mongo_URL;
+mongoose
+  .connect(mongoURL)
+  .then(() => {
+    app.listen(() => {
+      console.log(`Listening on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(`${err}, could not connect`));
