2b2ecf5b7ce5e8f39aecc104f9394ba01d9a54f1
Nugar <cladeadenugar@gmail.com>
2023-11-23 17:39:47 +0100

    added

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,4 +38,20 @@ export const register = async (req, res) => {
 };
 
 //log in
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist" });
 
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch)
+      return res.status(400).json({ msg: "Invalid password" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
